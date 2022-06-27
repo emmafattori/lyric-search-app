c88303902c454e105ddf3b3ef55146310c34bf24
Emma Fattori <emma@applydigital.co>
2022-06-27 09:30:44 -0400

    setting up layout and navbar components

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = function Navbar() {
+    return (
+        <nav className="navbar navbar-dark bg-dark mb-5">
+            span.navbar
+        </nav>
+    )
+}
+
+export default Navbar;
