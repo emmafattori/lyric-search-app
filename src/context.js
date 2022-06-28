7274b949579ba5ddd6f691d1be0e0bdbce02a3bc
Emma Fattori <emma@applydigital.co>
2022-06-28 09:47:41 -0400

    changing from spotify to genius api

--- src/context.js
+++ src/context.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 const Context = React.createContext();
 
 
-const baseUrl = 'https://spotify23.p.rapidapi.com/search/'
-
+const baseUrl = 'https://api.genius.com/oauth/authorize?'
+const client_id = process.env.REACT_APP_GENIUS_CLIENTID
 
 export class Provider extends Component {
     state = {
@@ -14,30 +14,20 @@ export class Provider extends Component {
         heading: 'Top 10 Tracks'
     };
     componentDidMount() {
-
-        const options = {
-            method: 'GET',
-            url: baseUrl,
-            params: {
-                q: 'the weeknd',
-                type: 'multi',
-                offset: '0',
-                limit: '10',
-                numberOfTopResults: '5'
-            },
-            headers: {
-                'X-RapidAPI-Host': 'spotify23.p.rapidapi.com',
-                // to do, hold in .env
-                'X-RapidAPI-Key': '8780038684mshf933dce4ebe00dfp171953jsn97e90579acca',
-
+        axios
+            .get(
+                'https://genius.p.rapidapi.com/search', {
+                params: { q: 'Kendrick Lamar' },
+                headers: {
+                    'X-RapidAPI-Key': '8780038684mshf933dce4ebe00dfp171953jsn97e90579acca',
+                    'X-RapidAPI-Host': 'genius.p.rapidapi.com'
+                }
             }
-        };
-
-        axios.request(options).then(function (response) {
-            console.log(response.data);
-        }).catch(function (error) {
-            console.error(error);
-        });
+            )
+            .then(res => {
+                console.log(res.data);
+            })
+            .catch(err => console.log(err));
     }
 
     render() {
